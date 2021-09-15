e4848f259fc4b0bb097b34011be2c2ea40f68a0f
Mphathisi <mphathisimatinise16@gmail.com>
2021-09-16 01:21:10 +0200

    Updates

---
+++ precache-manifest.c9cbb8ac6cda0f432a48fbc3fb00bf38.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6951962a7fd34c72dfd475ec1cfc9c0e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2fc4cf21ab7c9a919d8",
+    "url": "/static/css/2.7285519e.chunk.css"
+  },
+  {
+    "revision": "a0481f82c87ad94b2870",
+    "url": "/static/css/main.bcf1dad3.chunk.css"
+  },
+  {
+    "revision": "e2fc4cf21ab7c9a919d8",
+    "url": "/static/js/2.9cf03417.chunk.js"
+  },
+  {
+    "revision": "70b070bc4c4b879d3d3b9683ea8bec9f",
+    "url": "/static/js/2.9cf03417.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0481f82c87ad94b2870",
+    "url": "/static/js/main.5af2455c.chunk.js"
+  },
+  {
+    "revision": "7ad5b0c1347f9ef5d109",
+    "url": "/static/js/runtime-main.cc58f57b.js"
+  },
+  {
+    "revision": "17f59ab4e5cae3e3e40bcb4b160692fe",
+    "url": "/static/media/me.17f59ab4.JPG"
+  },
+  {
+    "revision": "b55163ca4322e5c8c0093cd999719763",
+    "url": "/static/media/song.b55163ca.mp3"
+  }
+]);
