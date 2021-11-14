aa7866891c9ca5b31366d136c6100bc82d19eef4
Mphathisi Dayimani <61159732+Mphathisi@users.noreply.github.com>
2021-11-14 06:33:43 +0200

    Update Intro.js

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -39,9 +39,10 @@ class Intro extends React.Component {
         <FadeInSection>
           <div className="intro-subtitle">#TheFullStackDeveloper</div>
           <div className="intro-desc">
-            I am a software engineer based in South Africa. I have profound
-            interest in mobile development, full stack development, machine learning, Operating Systems,  
-            and everything in between.
+           I am a software developer that has a strong interest in software development. I enjoy writing code, solving problems, and building softwares.
+           Throughout the journey as a software developer, I have used many technologies such as JavaScript, Node.JS, Java for server side and Angular
+           and React for building front-end web applications and firebase, MongoDB, MYSQL databases, GitHub version control. Flutter and ionic framework 
+           for cross-platform mobile apps. I am a person who is flexible to learn any programming language that I come across.
           </div>
           <a
             href="mailto:mphathisimatinise16@gmail.com"
